39c00bb2e8a1dcc1c6e1d86a245ef52347a6605c
sibonjic.haris@gmail.com <sibonjic.haris@gmail.com>
2015-10-07 20:38:22 +0200

    finished version

--- esp_pir_sensor.ino
+++ esp_pir_sensor.ino
@@ -1,5 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <Time.h> 
+
+const int timeZone = 1;     // Central European Time
+//const int timeZone = -5;  // Eastern Standard Time (USA)
+//const int timeZone = -4;  // Eastern Daylight Time (USA)
+//const int timeZone = -8;  // Pacific Standard Time (USA)
+//const int timeZone = -7;  // Pacific Daylight Time (USA)
 
 #define INPUT_PIN 12
 
@@ -11,8 +18,7 @@ const char* password = "nemainterneta";
 IPAddress timeServerIP;                         // time.nist.gov NTP server address
 unsigned int localPort = 2390;                  // local port to listen for UDP packets
 const char* ntpServerName = "time.nist.gov";
-const int NTP_PACKET_SIZE = 48;                 // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE];            // buffer to hold incoming and outgoing UDP packets
+
 WiFiUDP udp;                                    // A UDP instance to let us send and receive packets over UDP
 
 WiFiClient client;                              // Use WiFiClient class to create TCP connections
@@ -20,9 +26,6 @@ WiFiClient client;                              // Use WiFiClient class to creat
 /*  GadgetKeeper Credentials                                                                    *
  *  To obtain the Thing Id, see instructions http://docs.gadgetkeeper.com/display/docs/Thing+Id */
 String thing_ID = "d5b7ff9c6c3511e5accf2f3e5a24aa78";
-
-/*  To obtain the Property Id see instructions http://docs.gadgetkeeper.com/display/docs/Property+Id */
-String property_ID = "f64c25bf6c3511e5accf2f3e5a24aa78";
 
 /*  To obtain the Event Id see instructions http://wiki.gadgetkeeper.com/display/docs/Event+Id  */
 String event_ID ="b34679d36cec11e5accf2f3e5a24aa78";
@@ -68,59 +71,59 @@ void setup() {
 }
  
 void loop() {
-  
+
   if(digitalRead(INPUT_PIN)==HIGH) {
-    //getTimeDate();  
     Serial.println("motion detected!");
-    //sendMotionData();
-    motionTrigerEvent(); 
-    delay(2000);
+    setSyncProvider(getNtpTime);    //get correct time
+    motionTrigerEvent();            //send Tigger Request
+    delay(2000);                    //this delay (2s) is here so that the PIR doesnt send continously packets
   }
 
-      while(client.available()){
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-    }
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
 }
 
 void motionTrigerEvent(){
+  //first we connect to GadgetKeeper Cloud Platform
   const int httpPort = 80;
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
   }
+    /*  in this section here we are creating the POST Request for GadgetKeeper event trigger                                  *
+     *  for more information about events and triggers see http://docs.gadgetkeeper.com/display/docs/Create+Event+And+Trigger */
+    String Date = String(year())+ "-" + printDigits(String(month()))+"-"+printDigits(String(day()));
+    String Time = printDigits(String(hour())) + ":"+ printDigits(String(minute())) +":"+printDigits(String(second()));
     client.print("POST " + event_url + " HTTP/1.1\r\n");
     client.print("Host: " + String(host) + "\r\n");
     client.print("X-ApiKey: " + String(api_key) + "\r\n");
     client.print("Content-Type: application/json; charset=UTF-8\r\n");
     client.print("Cache-Control: no-cache\r\n");
-    client.print("Content-Length: 48\r\n");
-    client.print("\n[{\"value\":true,\"at\":\"2013-06-02T21:05:04.150Z\"}]\r\n");
-    delay(100);
-
-}
-
-void sendMotionData(){
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-    client.print("PUT /v1/things/d5b7ff9c6c3511e5accf2f3e5a24aa78/properties/f64c25bf6c3511e5accf2f3e5a24aa78/value.json HTTP/1.1\r\n");
-    client.print("Host: " + String(host) + "\r\n");
-    client.print("X-ApiKey: " + String(api_key) + "\r\n");
-    client.print("Content-Type: application/json; charset=UTF-8\r\n");
-    client.print("Cache-Control: no-cache\r\n");
-    client.print("Content-Length: 4\r\n");
-    client.print("\ntrue\r\n");
+    client.print("Content-Length: 50\r\n");
+    client.print("\n[{\"value\":true,\"at\":\""+ Date +"T" + Time +".000Z\"}]\r\n");
     delay(100);
 }
 
-void getTimeDate(){
+// this fucntion add 0 to time digits 0-9
+String printDigits(String digits){
+  if(digits.length() < 2)
+    return 0+digits;
+  return digits;
+}
+
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48;          // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE];     // buffer to hold incoming and outgoing UDP packets
+
+time_t getNtpTime(){
   WiFi.hostByName(ntpServerName, timeServerIP); 
 
   sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-                                 // wait to see if a reply is available
+                               // wait to see if a reply is available
   delay(1000);
   
   int cb = udp.parsePacket();
@@ -141,40 +144,13 @@ void getTimeDate(){
     // combine the four bytes (two words) into a long integer
     // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
+//    Serial.print("Seconds since Jan 1 1900 = " );
+//    Serial.println(secsSince1900);
+    return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
   }
 }
 
-void sendRequest(String method, String URL){
-  
-}
+
 
 // send an NTP request to the time server at the given address
 unsigned long sendNTPpacket(IPAddress& address)
